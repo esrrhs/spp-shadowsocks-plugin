90a1f4b65d06de3336d718e5794171f9ca7b7800
esrrhs <esrrhs@163.com>
2020-10-08 17:52:32 +0800

    add

--- main.go
+++ main.go
@@ -17,10 +17,10 @@ import (
 func main() {
 	log_init()
 
-	log.Printf("start plugin\n")
+	log.Printf("start plugin")
 
 	loggo.Ini(loggo.Config{
-		Level:     loggo.LEVEL_INFO,
+		Level:     loggo.LEVEL_ERROR,
 		Prefix:    "spp",
 		MaxDay:    3,
 		NoLogFile: true,
@@ -35,11 +35,11 @@ func main() {
 	for i := 0; i < ss.NumField(); i++ {
 		name := typeOfT.Field(i).Name
 		if ss.Field(i).Kind() == reflect.Int {
-			usage += fmt.Sprintf("%v = %v\n", strings.ToLower(name), ss.Field(i).Int())
+			usage += fmt.Sprintf("%v = %v", strings.ToLower(name), ss.Field(i).Int())
 		} else if ss.Field(i).Kind() == reflect.String {
-			usage += fmt.Sprintf("%v = %v\n", strings.ToLower(name), ss.Field(i).String())
+			usage += fmt.Sprintf("%v = %v", strings.ToLower(name), ss.Field(i).String())
 		} else if ss.Field(i).Kind() == reflect.Bool {
-			usage += fmt.Sprintf("%v = %v\n", strings.ToLower(name), ss.Field(i).Bool())
+			usage += fmt.Sprintf("%v = %v", strings.ToLower(name), ss.Field(i).Bool())
 		}
 	}
 
@@ -47,23 +47,23 @@ func main() {
 		fmt.Printf(usage)
 	}
 	v := flag.Bool("V", false, "")
-	log.Printf("parse args\n")
+	log.Printf("parse args")
 	flag.Parse()
-	log.Printf("vpn mode %v\n", *v)
+	log.Printf("vpn mode %v", *v)
 
-	log.Printf("parse env\n")
+	log.Printf("parse env")
 	opts, err := parseEnv()
 	if err != nil {
-		log.Printf("parseEnv fail %v\n", err)
+		log.Printf("parseEnv fail %v", err)
 		os.Exit(-11)
 	}
 
 	remoteaddr, _ := opts.Get(strings.ToLower("remoteaddr"))
 	localaddr, _ := opts.Get(strings.ToLower("localaddr"))
-	log.Printf("remoteaddr %v\n", remoteaddr)
-	log.Printf("localaddr %v\n", localaddr)
+	log.Printf("remoteaddr %v", remoteaddr)
+	log.Printf("localaddr %v", localaddr)
 
-	log.Printf("set config\n")
+	log.Printf("set config")
 	for i := 0; i < ss.NumField(); i++ {
 		name := typeOfT.Field(i).Name
 		if value, b := opts.Get(strings.ToLower(name)); b {
@@ -72,15 +72,15 @@ func main() {
 					x, _ := strconv.Atoi(value)
 					if !ss.Field(i).OverflowInt(int64(x)) {
 						ss.Field(i).SetInt(int64(x))
-						log.Printf("%v = %v\n", name, x)
+						log.Printf("%v = %v", name, x)
 					}
 				} else if ss.Field(i).Kind() == reflect.String {
 					ss.Field(i).SetString(value)
-					log.Printf("%v = %v\n", name, value[0])
+					log.Printf("%v = %v", name, value[0])
 				} else if ss.Field(i).Kind() == reflect.Bool {
 					x := value == "true"
 					ss.Field(i).SetBool(x)
-					log.Printf("%v = %v\n", name, x)
+					log.Printf("%v = %v", name, x)
 				}
 			}
 		}
@@ -97,23 +97,23 @@ func main() {
 
 	ty, _ := opts.Get("type")
 	if len(ty) > 0 && ty == "server" {
-		log.Printf("start server\n")
+		log.Printf("start server")
 		_, err := proxy.NewServer(config, protos, []string{remoteaddr})
 		if err != nil {
-			log.Printf("NewServer fail %v\n", err)
+			log.Printf("NewServer fail %v", err)
 			os.Exit(-12)
 		}
-		log.Printf("start server ok\n")
+		log.Printf("start server ok")
 	} else {
-		log.Printf("start client\n")
+		log.Printf("start client")
 		_, err := proxy.NewClient(config, protos[0], remoteaddr, common.UniqueId(),
 			"ss_proxy",
 			[]string{"tcp"}, []string{localaddr}, []string{""})
 		if err != nil {
-			log.Printf("NewClient fail %v\n", err)
+			log.Printf("NewClient fail %v", err)
 			os.Exit(-13)
 		}
-		log.Printf("start client ok\n")
+		log.Printf("start client ok")
 	}
 
 	for {
